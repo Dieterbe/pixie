c092164562719ff36a9c95327558fed2e7b607ed
Dieter Plaetinck <dieter@plaetinck.be>
2013-11-05 23:11:56 -0500

    basic rough prototype wip

--- main.go
+++ main.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-	"encoding/json"
-	"io"
-	"fmt"
-	"strings"
-	"mime"
-	"path/filepath"
 	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"github.com/stvp/go-toml-config"
+	"io"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"path/filepath"
+	"strings"
 )
 
+var thumbnail_dir = config.String("thumbnail_dir", "")
+
 type Photo struct {
-	Name string `json:"name"`
-	AbsPath string `json:"abspath"`
-	PathMd5 string `json:"path_md5"`
+	Name  string `json:"name"`
+	Dir   string `json:"dir"`
+	Thumb string `json:"thumb"`
 }
 
 func (p *Photo) String() string {
-	return fmt.Sprintf("Photo {Name: '%s', AbsPath: '%s', PathMd5: '%s'}", p.Name, p.AbsPath, p.PathMd5)
+	return fmt.Sprintf("Photo {Name: '%s', Dir: '%s', Thumb: '%s'}", p.Name, p.Dir, p.Thumb)
 }
 
-func api_handler (w http.ResponseWriter, r *http.Request) {
-	if ! strings.HasPrefix(r.URL.Path, "/api/photos/dir=") {
+func api_handler(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, "/api/photos/dir=") {
 		return
 	}
 	dir := strings.Replace(r.URL.Path, "/api/photos/dir=", "", 1)
@@ -33,7 +36,7 @@ func api_handler (w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("Cannot read directory: '%s': %s", dir, err), 503)
 	}
 	photos := make([]Photo, 0, len(list))
-	abspath, err := filepath.Abs(dir)
+	dir, err = filepath.Abs(dir)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Cannot figure out directory abspath: '%s': %s", dir, err), 503)
 	}
@@ -43,9 +46,9 @@ func api_handler (w http.ResponseWriter, r *http.Request) {
 		mime := mime.TypeByExtension(ext)
 		if strings.HasPrefix(mime, "image/") {
 			h := md5.New()
-			io.WriteString(h, fmt.Sprintf("file://%s/%s", abspath, name))
-			pathmd5 := fmt.Sprintf("%x", h.Sum(nil))
-			p := Photo{name, abspath, pathmd5}
+			io.WriteString(h, fmt.Sprintf("file://%s/%s", dir, name))
+			thumb := fmt.Sprintf("%x.png", h.Sum(nil))
+			p := Photo{name, dir, thumb}
 			photos = append(photos, p)
 		}
 	}
@@ -57,9 +60,11 @@ func api_handler (w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main () {
+func main() {
 	addr := ":8080"
+	config.Parse("config.ini")
 	http.HandleFunc("/api/", api_handler)
+	http.Handle("/thumbnails/", http.StripPrefix("/thumbnails/", http.FileServer(http.Dir(*thumbnail_dir))))
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Printf("starting up on %s\n", addr)
 	http.ListenAndServe(addr, nil)
