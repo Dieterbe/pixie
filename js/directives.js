9e40974e9c513c8dd2ae0965fc2378cc3ac682a0
Dieter Plaetinck <dieter@plaetinck.be>
2013-12-26 17:49:03 -0500

    cleanup

--- js/directives.js
+++ js/directives.js
@@ -4,14 +4,11 @@
 angular.module('directives', []).
  // http://stackoverflow.com/questions/12790854/angular-directive-to-scroll-to-a-given-item
 directive('scrollIf', function () {
-    console.log("scroll top level");
     return {
         link: function (scope, element, attributes) {
-            console.log("scroll function cb");
             setTimeout(function () {
                 scope.$watch(attributes.scrollIf, function(val) {
                     if (val) {
-                        console.debug("scrolling to #photo-" + scope.$parent.focusIndex);
                         //window.scrollTo(0, element.offsetTop - 200)
                         //window.scrollTo(0, $("#photo-"+ scope.$parent.focusIndex).offsetTop);
                         window.scrollTo(0, document.getElementById("photo-"+ scope.$parent.focusIndex).offsetTop - 200);
